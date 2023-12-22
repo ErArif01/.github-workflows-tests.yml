16e5d472888a6f59a513f9068e647fb96ec9463c
ER MOHD ARIF <141546622+ErArif01@users.noreply.github.com>
2023-12-22 10:53:40 +0530

    Create test_my_app.py

---
+++ .github/workflows/test_my_app.py
@@ -0,0 +1,16 @@
+# test_my_app.py
+
+def add(x, y):
+    return x + y
+
+def test_addition():
+    assert add(1, 2) == 3
+
+def test_subtraction():
+    assert add(5, 3) == 8
+
+def test_multiplication():
+    assert add(2, 2) == 4
+
+def test_division():
+    assert add(8, 2) == 10
